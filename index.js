995ea298ff30227ebc4c35bcc399ca61358602f0
Clara Pons <clarapons@MacBook-Air-de-Clara.local>
2022-06-20 16:42:38 +0200

    number exercices

---
+++ index.js
@@ -0,0 +1,64 @@
+// 01 - Number
+//const integer = 102
+//const float = 13.9
+//console.log(integer)
+//console.log(float)
+
+// 02 - Convert
+//const basic = 34
+//const stringified = basic.toString()
+//console.log(typeof(stringified))
+//console.log(stringified)
+
+// 03 - Round
+//const num = 1.5    
+//const rounded = Math.round(num)
+//console.log(rounded)
+
+// 04 - Arithmétique
+// const test = 12 
+// const bis = 5
+
+// const addition = test + bis
+// console.log(addition)
+
+// const soustraction = test - bis
+// console.log(soustraction)
+
+// const division = test / bis 
+// console.log(division)
+
+// const multiplication = test * bis 
+// console.log(multiplication)
+
+// const modulo = test % bis
+// console.log(modulo)
+
+// 05 - Reste division euclidienne
+//  const moduloOne = 999 % 1
+//  console.log(moduloOne)
+
+//  const moduloTwo = 25 % 1
+//  console.log(moduloTwo)
+
+//  const moduloThree = 25 % 5
+//  console.log(moduloThree)
+
+// const moduloFour = 190280 % 1
+// console.log(moduloFour)
+
+// const moduloFive = 190280 % 2
+// console.log(moduloFive)
+
+// const moduloSix = 190280 % 4
+// console.log(moduloSix)
+
+// const moduloSeven = 190280 % 5
+// console.log(moduloSeven)
+
+// const moduloEight = 47 % 1
+// console.log(moduloEight)
+
+// const moduloNine = 691 % 1
+// console.log(moduloNine)
+
